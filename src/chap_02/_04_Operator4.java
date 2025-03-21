22b0ce98bc9eabba582f066fce249f2fbb4580dd
JAEMOON LEE <ljmm5721@naver.com>
2025-03-21 16:03:31 +0900

    first commit

--- src/chap_02/_04_Operator4.java
+++ src/chap_02/_04_Operator4.java
@@ -15,7 +15,16 @@ public class _04_Operator4 {
         System.out.println((5 > 3) && (3 < 1)); // 5는 3보다 크고, 3은 1보다 작다 (false)
 
         // Or 연산
-        System.out.println((5 > 3) || (3 > 1)); // 5는 3보다 크고, 3은 1보다 크다 (true)
-        System.out.println((5 > 3) || (3 < 1)); // 5는 3보다 크고, 3은 1보다 작다 (true)
+        System.out.println((5 > 3) || (3 > 1)); // 5는 3보다 크거나, 3은 1보다 크다 (true)
+        System.out.println((5 > 3) || (3 < 1)); // 5는 3보다 크거나, 3은 1보다 작다 (true)
+        System.out.println((5 < 3) || (3 < 1)); // 5는 3보다 작거나, 3은 1보다 작다 (false)
+
+        // System.out.println(1< 3< 5); 연속적으로 적으면 오류 발생!!!!!
+
+        // 논리 부정 연산자
+        System.out.println(!true); // false
+        System.out.println(!false); // true
+        System.out.println(!(5 == 5)); // false
+        System.out.println(!(5 == 3)); // true
     }
 }
